9a5ba3741a41c745605afb0ef0678ddc0ff7bf0b
993178 <papilio29@hotmail.com>
2019-02-01 20:57:43 +0100

    meer geconfigureer

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller 
 import willekeurigGetal from './test';
 
-console.log(`I imported ${willekeurigGetal} from another module!!1!`);
+console.log(`I imported ${willekeurigGetal} from another module called test.js!!1!`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,38 @@
 // hier vertellen we webpack wat ie precies moet omzetten en samenvoegen tot wat
 const path = require('path');   // we hebben een absoluut path nodig
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',   // hier begint webpack met packen
     output: {
-        path: path.resolve(__dirname, 'dist/js'),           // we gebruiken een path method uit het ding van regel 2 om naar de juiste map te verwijzen. __dirname is the current absolute path, en die koppelen we aan de map waar we de bundel in willen hebben: dist/js
-        filename: 'bundle.js'   // standaardnaam
+        path: path.resolve(__dirname, 'dist'),           // we gebruiken een path method uit het ding van regel 2 om naar de juiste map te verwijzen. __dirname is the current absolute path, en die koppelen we aan de map waar we de bundel in willen hebben: dist/js
+        filename: 'js/bundle.js'   // standaardnaam. Webpack zet dit zelf in een <script> in het html-bestand dat hieronder in de plugin wordt gemaakt
+    }, 
+    devServer: {
+        contentBase: './dist',    // de map met bestanden die we via de server willen monitoren tijdens het programmeren (maw het eindproduct)
+    },
+    plugins: [
+        new HtmlWebpackPlugin({     // we willen de src-html kopiëren naar de distmap en er automatisch het juiste script source bestand in laten gooien
+            filename: 'index.html',         // doelbestand, wordt hier gemaakt 
+            template: './src/index.html'      // bronbestand. Sommige apps hebben dit niet eens maar laten hem genereren door webpack...
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,      // 'zoek naar alle bestanden die eindigen$ op .js'
+                exclude: /node_modules/,    // 'dwz alle .js-bestanden behalve die in de node_modules'. Soort .gitignore
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
     }
-}   // we missen nog loaders en plugins, met entry en output de 4 noodzakelijke onderdelen van de webpackconfiguratie
+}   // entry, output, loaders en plugins zijn de 4 noodzakelijke onderdelen van de webpackconfiguratie
 // ook nodig: webpack toevoegen als apart script in de package.json zodat ie dit ding gaat zoeken en doet wat de bedoeling is
 // ook nodig: installeren van het pakket waarmee je daadwerkelijk iets met webpack kunt doen...: webpack-cli --save-dev (want het is een devtool dat je in je package.json vermeld wilt hebben)
 
 // na output en voor de loaders en plugins hadden we nog mode: 'development' om snel code te verwerken terwijl je bezig bent met bouwen, terwijl andere optie production voor het grondige, uiteindelijke proces is met minifying en weet ik wat meer om het te uploaden pakket zo klein mogelijk te houden
 // maar het is makkelijker om mode niet hier te zetten maar in het npm script in de package.json zodat je het niet op het eind hoeft te veranderen
+
+// als je het start script runt, krijg je de output van die plugin niet te zien, veranderingen worden rechtstreeks doorgegeven aan de server. Pas met dev (of build) worden de bundle.js en index.html in de distmap daadwerkelijk gemaakt
