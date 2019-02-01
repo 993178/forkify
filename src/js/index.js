21caea82fa5bcaa3f5dff5063d754af611736277
993178 <papilio29@hotmail.com>
2019-02-01 22:38:30 +0100

    searchbestanden erbij

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,1 @@
 // Global app controller 
-import willekeurigGetal from './test';
-const test = 666
-console.log(`I quote ${test} and even imported ${willekeurigGetal} from another module called test.js!!1!`);
